eb70d3b9dbfc2d7b82cfef5a76180b1c179d6788
douglasemiliano <emaildedouglas@gmail.com>
2018-10-31 17:34:19 -0600

    Templates

---
+++ src/app/client/client.ts
@@ -0,0 +1,11 @@
+export class Client {
+    constructor(
+    public name: string,
+    public telefone: string,
+    public dataNascimento: string,
+    public endereco: string,
+    public email: string,
+    public cpf: string
+    ){}
+
+}
